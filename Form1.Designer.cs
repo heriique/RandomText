d9dea8b69447eef80431f968bb1e1c5fe8f29f24
ercr1608 <ercr1608@WIN-LKQN31MS8DB>
2023-02-06 08:50:21 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace RandomText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Velg et spørsmål!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(154, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(38, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "9C";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(348, 18);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "9D";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 298);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Klimaspørsmål";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace RandomText
+{
+    
+    public partial class Form1 : Form
+    {
+        string[] textlist = {"Hva gjør mennesker som påvirker klimaet?",
+                            "Hva er drivhusgasser/klimagasser?",
+                            "Hva er drivhuseffekten?",
+                            "Fortell om en internasjonal avtale om klima.",
+                            "Hva gjør FNs klimapanel (IPCC)?",
+                            "Hvorfor stiger den globale gjennomsnittstemperaturen?",
+                            "Hvorfor stiger havet?",
+                            "Gi et eksempel på en positiv eller negativ tilbakekobling.",
+                            "Hva kan du gjøre for å leve mer klimavennlig?",
+                            "Hva betyr karbonavtrykk?",
+                            "(9C) Hvorfor smelter isen?",
+                            "(9C) Hva er Paris-avtalen?",
+                            "(9C) Hva kan du gjøre for å hindre at isen smelter?",
+                            "(9C) Hvordan kan du minske forbruket ditt?",
+                            "(9C) Hvordan kan du minske CO2-avtrykket ditt?",
+                            "(9D) Hva er det som bryter ned ozonlaget?",
+                            "(9D) Hvordan hjelper gjenbruk av klær klimaet?",
+                            "(9D) Hvor mye har gjennomsnittstemperaturen på jorda økt siden 1750?",
+                            "(9D) Hvordan er det bra for klimaet at vi kjører mindre bil og fly?",
+                            "(9D) Hvordan går det med ozonlaget nå?",
+                            "(9D) Hva vil skje hvis polene smelter?"};
+        int clicks = 0;
+        int n = 0;
+        const int base_count = 10;
+        const int c_count = 5;
+        const int d_count = 6;
+        int[] indices;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            setup();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (clicks % n == 0)
+            {
+                setup();
+            }
+            label1.Text = (indices[clicks] + 1).ToString() + ": " + textlist[indices[clicks++ % n]];
+        }
+
+        void setup()
+        {
+            //n = textlist.Length;
+            bool c = radioButton1.Checked;
+            n = base_count + (c ? c_count : d_count);
+            System.Console.WriteLine("n= " + n);
+            indices = new int[n];
+            int i = 0;
+            for (; i < base_count; i++)
+                indices[i] = i;
+            if (c)
+                for (; i < base_count + c_count; i++)
+                    indices[i] = i;
+            else
+                for (; i < base_count + d_count; i++)
+                    indices[i] = i + c_count;
+            System.Console.Write("indices= ");
+            for (int j = 0; j < indices.Length; j++)
+                System.Console.Write(indices[j] + ", ");
+            System.Console.WriteLine();
+            random.Shuffle(indices);
+            System.Console.Write("indices= ");
+            for (int j = 0; j < indices.Length; j++)
+                System.Console.Write(indices[j] + ", ");
+            System.Console.WriteLine();
+            clicks = 0;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                radioButton2.Checked = false;
+            setup();   
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+                radioButton1.Checked = false;
+            setup();
+        }
+    }
+    static class RandomExtensions
+    {
+        public static void Shuffle<T>(this Random rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
+}
